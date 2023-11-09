9be04fb8aaecc2ce7377d4355cdd69b99d929e95
Angus <angus.duffy.2021@uni.strath.ac.uk>
2023-11-09 14:57:12 +0000

    Adding QuickCheck Tests for JSONOutput

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,18 @@
 import System.Exit
+import Test.QuickCheck 
+import JSON 
+import JSONOutput (renderJSON)
+
+
+
+propReverse :: [Int] -> Bool 
+propReverse xs = reverse (reverse xs) == xs 
+
+-- JSON Output test --
+renderJSONTest :: [JSON] -> Bool 
+renderJSONTest xs = 
+
+
 
 main :: IO ()
-main = do putStrLn "Test suite not yet implemented"
-          exitFailure
+main = do quickCheck renderJSONTest 
